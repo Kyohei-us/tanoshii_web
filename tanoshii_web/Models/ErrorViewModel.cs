eafd0cc827a1762bdfa07028571ebffe0f79ea43
Kyohei-us <kyohei1212000@gmail.com>
2022-10-10 16:24:57 +0900

    Initial Commit

---
+++ tanoshii_web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace tanoshii_web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
